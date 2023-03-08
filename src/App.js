9ba90c0f291b68c839518c255e3c35969a4b692c
superman <goodnesskolapo@gmail.com>
2023-03-08 14:50:18 +0100

    not yet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Navbar from "./components/Navbar";
 import './App.css';
+import StakingForm from "./pages/admin.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TokenList from "./pages/home.jsx";
 
 function App() {
 
 
   return (
     <div className="App">
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
 
-    
+      <Routes>
+        <Route path="/" element={<TokenList />} />
+      </Routes>
+    </BrowserRouter>
+
+    <StakingForm />
     </div>
   );
 }
