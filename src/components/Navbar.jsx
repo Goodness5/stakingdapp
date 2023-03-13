89984889dc75e97efa713307487eb384b2196d9e
Goodness5 <goodnesskolapo@gmail.com>
2023-03-13 18:01:18 +0100

    hmmm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ConnectionButton from './ConnectionButton'
+import React from "react";
+import { Link } from "react-router-dom";
+import ConnectionButton from "./ConnectionButton";
 
 const Navbar = () => {
   return (
-    <div className='flex justify-end bg-black py-3 border-b border-gray-300'>
-
-        <ConnectionButton />
-        
+    <div className="flex justify-end bg-black py-3 border-b border-gray-300">
+      <ConnectionButton />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
