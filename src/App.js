cac09464f52ba5c49bea2e6ea87bc9b7380e8dc5
Goodness5 <goodnesskolapo@gmail.com>
2023-03-13 18:02:42 +0100

    hmmm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import Navbar from "./components/Navbar";
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import  Stake from "./pages/stake.jsx";
+import Stake from "./pages/stake.jsx";
 import TokenList from "./pages/home.jsx";
 
 function App() {
-
-
   return (
     <div className="App">
-    <BrowserRouter>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<TokenList />} />
-        <Route path="/stake" element={<Stake />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TokenList />} />
+          <Route path="/stake" element={<Stake />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
